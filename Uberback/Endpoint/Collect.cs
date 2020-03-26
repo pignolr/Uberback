7a48fda31ba9e1bb49e37a4399fb91eb8dbe73e2
Rémi Pignol <remi.pignol@epitech.eu>
2020-03-26 15:55:09 +0900

    Add Validator for endpoint Collect + remove of pagination of Collect

--- Uberback/Endpoint/Collect.cs
+++ Uberback/Endpoint/Collect.cs
@@ -19,18 +19,9 @@ namespace Uberback.Endpoint
             {
                 var args = Common.ParseArgs(Request.Body);
 
-                // Error Handling
-                Common.Answer? error = Common.BasicCheck(args.Get("token"));
-                if (error.HasValue)
-                    return (Response.AsJson(new Response.Error()
-                    {
-                        Message = error.Value.message
-                    }, error.Value.code));
-                if (!string.IsNullOrEmpty(args.Get("type")) && args.Get("type") != "image" && args.Get("type") != "text")
-                    return (Response.AsJson(new Response.Error()
-                    {
-                        Message = "If set, type must be text or image"
-                    }, HttpStatusCode.BadRequest));
+                Validator.Collect.ValidatorResponse error = null;
+                if ((error = Validator.Collect.ValidateRequest(args)) != null)
+                    return Response.AsJson(new Response.Error() { Message = error.Message }, error.StatusCode);
 
                 // Getting datas
                 List<Response.Data> datas;
@@ -60,32 +51,14 @@ namespace Uberback.Endpoint
                 // from/to filters
                 if (!string.IsNullOrEmpty(args.Get("from")))
                 {
-                    DateTime from;
-                    if (!DateTime.TryParseExact(args.Get("from"), "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out from))
-                    {
-                        return (Response.AsJson(new Response.Error()
-                        {
-                            Message = "From must be in the format yyyyMMdd"
-                        }, HttpStatusCode.BadRequest));
-                    }
+                    DateTime from = DateTime.ParseExact(args.Get("from"), "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None);
                     datas.RemoveAll(y => DateTime.ParseExact(y.DateTime, "yyyyMMddHHmmss", CultureInfo.InvariantCulture) < from);
                 }
                 if (!string.IsNullOrEmpty(args.Get("to")))
                 {
-                    DateTime to;
-                    if (!DateTime.TryParseExact(args.Get("to"), "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out to))
-                    {
-                        return (Response.AsJson(new Response.Error()
-                        {
-                            Message = "To must be in the format yyyyMMdd"
-                        }, HttpStatusCode.BadRequest));
-                    }
+                    DateTime to = DateTime.ParseExact(args.Get("to"), "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None);
                     datas.RemoveAll(y => DateTime.ParseExact(y.DateTime, "yyyyMMddHHmmss", CultureInfo.InvariantCulture) > to);
                 }
-
-                var paginationError = ExtractDataFromPagination(args, datas);
-                if (paginationError != null)
-                    return paginationError;
 
                 Dictionary<string, FlagData[]> finalDatas = new Dictionary<string, FlagData[]>();
                 Dictionary<string, Dictionary<string, double>> flags = new Dictionary<string, Dictionary<string, double>>();
@@ -136,48 +109,6 @@ namespace Uberback.Endpoint
                     Datas = finalDatas
                 }));
             });
-        }
-
-        private Nancy.Response ExtractDataFromPagination(System.Collections.Specialized.NameValueCollection args, List<Response.Data> datas)
-        {
-            int nbElem = -1;
-            int page = 1;
-            if (!string.IsNullOrEmpty(args.Get("nbelem")))
-            {
-                if (!int.TryParse(args.Get("nbelem"), out nbElem) || (nbElem <= 0 && nbElem != -1))
-                {
-                    return (Response.AsJson(new Response.Error()
-                    {
-                        Message = "Nbelem must be a positive integer"
-                    }, HttpStatusCode.BadRequest));
-                }
-            }
-            if (!string.IsNullOrEmpty(args.Get("page")))
-            {
-                if (!int.TryParse(args.Get("page"), out page) || page <= 0)
-                {
-                    return (Response.AsJson(new Response.Error()
-                    {
-                        Message = "Page must be a positive integer"
-                    }, HttpStatusCode.BadRequest));
-                }
-                if (nbElem == -1)
-                    nbElem = 20;
-                if (datas.Count() <= (page - 1) * nbElem)
-                {
-                    return (Response.AsJson(new Response.Error()
-                    {
-                        Message = "Page is Invalid"
-                    }, HttpStatusCode.BadRequest));
-                }
-            }
-            if (nbElem == -1)
-                return null;
-
-            datas.RemoveRange(0, (page - 1) * nbElem);
-            if (datas.Count() > nbElem)
-                datas.RemoveRange(nbElem, datas.Count() - nbElem);
-            return null;
         }
 
         /// <summary>
---
+++ Uberback/Validator/Collect.cs
@@ -0,0 +1,56 @@
+﻿using Nancy;
+using System;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace Uberback.Validator
+{
+    class Collect
+    {
+        public class ValidatorResponse
+        {
+            public string Message { get; set; }
+            public HttpStatusCode StatusCode { get; set; }
+        }
+
+        public static ValidatorResponse ValidateRequest(NameValueCollection args)
+        {
+            ValidatorResponse error;
+            if ((error = ValidateToken(args.Get("token"))) != null
+                || (error = ValidateType(args.Get("type"))) != null
+                || (error = ValidateFrom(args.Get("from"))) != null
+                || (error = ValidateTo(args.Get("to"))) != null)
+                return error;
+            return null;
+        }
+
+        public static ValidatorResponse ValidateToken(string token)
+        {
+            Endpoint.Common.Answer? error = Endpoint.Common.BasicCheck(token);
+            if (error.HasValue)
+                return new ValidatorResponse { Message = error.Value.message, StatusCode = error.Value.code };
+            return null;
+        }
+
+        public static ValidatorResponse ValidateType(string type)
+        {
+            if (!string.IsNullOrEmpty(type) && type != "image" && type != "text")
+                return new ValidatorResponse { Message = "If set, type must be text or image", StatusCode = HttpStatusCode.BadRequest };
+            return null;
+        }
+
+        public static ValidatorResponse ValidateFrom(string from)
+        {
+            if (!string.IsNullOrEmpty(from) && !DateTime.TryParseExact(from, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dummy))
+                return new ValidatorResponse { Message = "From must be in the format yyyyMMdd", StatusCode = HttpStatusCode.BadRequest };
+            return null;
+        }
+
+        public static ValidatorResponse ValidateTo(string to)
+        {
+            if (!string.IsNullOrEmpty(to) && !DateTime.TryParseExact(to, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dummy))
+                return new ValidatorResponse { Message = "To must be in the format yyyyMMdd", StatusCode = HttpStatusCode.BadRequest };
+            return null;
+        }
+    }
+}
