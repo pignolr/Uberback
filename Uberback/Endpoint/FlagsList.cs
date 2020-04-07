9543b610f31dacf3bc6e69e7fc4b369e8cd94ccc
Xwilarg <xwilarg@yahoo.fr>
2020-04-07 13:34:11 +0200

    Add /flagsList

---
+++ Uberback/Endpoint/FlagsList.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+
+namespace Uberback.Endpoint
+{
+    public class FlagsList : NancyModule
+    {
+        /// <summary>
+        /// Get information
+        /// </summary>
+        public FlagsList() : base("/flagsList")
+        {
+            Get("/", x =>
+            {
+                return (Response.AsJson(new Response.FlagsList()
+                {
+                    FlagsImage = new[] { "SAFE", "Adult", "Medical", "Racy", "Violence" },
+                    FlagsText = new[] { "SAFE", "SEVERE_TOXICITY", "IDENTITY_ATTACK", "INSULT", "PROFANITY", "THREAT", "INFLAMMATORY", "OBSCENE" }
+                }));
+            });
+        }
+    }
+}
---
+++ Uberback/Response/FlagsList.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Uberback.Response
+{
+    public class FlagsList
+    {
+        [JsonProperty]
+        public string[] FlagsImage;
+
+        [JsonProperty]
+        public string[] FlagsText;
+    }
+}
